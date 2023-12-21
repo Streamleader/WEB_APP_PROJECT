e580e5e83352051cbacb3cd0642fd19c76daf5bb
KAGISHO MABAKANE PITSI <10919953@mylife.unisa.ac.za>
2023-12-21 15:54:53 +0200

    add files

---
+++ app2.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import joblib
+
+
+# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("PHONE", "E-MAIL", "MESSAGE")
+
+
+if add_selectbox == 'PHONE'
+    a=st.number_input("ENTER YOUR PHONE NUMBER")
+
+
+mile=st.number_input('Enter a Mileage value:')
+age=st.number_input('Enter a age value:')
+
+
+if st.button('PREDICT'):
+
+    data=pd.DataFrame()
+    data['Mileage']=[mile]
+    data['Age(yrs)']=[age]
+
+    model=joblib.load('Car_selection_Model.joblib')
+
+    prediction=model.predict(data)
+
+    st.write(prediction)
+
+
+if st.button('PREDICT'):
